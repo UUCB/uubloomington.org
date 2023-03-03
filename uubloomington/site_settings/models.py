7506ebdda9238f1ff1896b7099070b9d858e1239
Hans Kelson <hans@communitycomputing.net>
2023-03-03 17:35:45 -0500

    Add common URLs to the upper right, fix header responsiveness again

--- uubloomington/site_settings/models.py
+++ uubloomington/site_settings/models.py
@@ -11,6 +11,9 @@ class SiteWideSettings(BaseGenericSetting):
     title = models.CharField(max_length=50, blank=True, null=True)
     subtitle = models.CharField(max_length=50, blank=True, null=True)
     tagline = models.CharField(max_length=200, blank=True, null=True)
+    giving_url = models.CharField(max_length=200, blank=True, null=True)
+    directions_url = models.CharField(max_length=2000, blank=True, null=True)
+    header_announcement = models.CharField(max_length=50, blank=True, null=True )
 
     churchcenter_calendar_url = models.CharField(max_length=900, blank=True, null=True, help_text='Church Center Calendar URL')
 
@@ -21,6 +24,9 @@ class SiteWideSettings(BaseGenericSetting):
             FieldPanel('title'),
             FieldPanel('subtitle'),
             FieldPanel('tagline'),
+            FieldPanel('giving_url'),
+            FieldPanel('directions_url'),
+            FieldPanel('header_announcement'),
         ], heading="Header Settings"),
         MultiFieldPanel([
             FieldPanel('churchcenter_calendar_url'),
