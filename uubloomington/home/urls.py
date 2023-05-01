61cf9db2b0f4225eb4e61ed428aadf36a969848f
Hans Kelson <hans@communitycomputing.net>
2023-05-01 12:29:35 -0400

    Stop auto-switching image carousel, add buttons instead

--- uubloomington/home/urls.py
+++ uubloomington/home/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('next_carousel_image/<int:current_image_pk>', views.next_carousel_image, name='next-carousel-image')
+    path('next_carousel_image/<int:current_image_pk>',
+         views.next_carousel_image,
+         kwargs={'mode': 'forward'},
+         name='next-carousel-image'
+         ),
+    path('previous_carousel_image/<int:current_image_pk>',
+         views.next_carousel_image,
+         kwargs={'mode': 'reverse'},
+         name='previous-carousel-image'
+         ),
 ]
--- uubloomington/home/views.py
+++ uubloomington/home/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render, get_object_or_404
 from .models import HomePageCarouselImages
 
 
-def next_carousel_image(request, current_image_pk):
+def next_carousel_image(request, current_image_pk, mode):
+    current_image = get_object_or_404(HomePageCarouselImages, pk=current_image_pk)
+    potential_images = current_image.page.carousel_images.all()
+    for index, image in enumerate(potential_images):
+        if current_image == {
+            'forward': current_image.page.carousel_images.last(),
+            'reverse': current_image.page.carousel_images.first(),
+        }[mode]:
+            return render(request, 'home/carousel_image.html', {
+                'carousel_image': {
+                    'forward': potential_images[0],
+                    'reverse': potential_images.last(),
+                }[mode],
+            })
+        elif current_image == image:
+            return render(request, 'home/carousel_image.html', {
+                'carousel_image': potential_images[index +
+                                                   {
+                                                       'forward': 1,
+                                                       'reverse': -1,
+                                                   }[mode]
+                                                   ],
+            })
+
+
+def previous_carousel_image(request, current_image_pk):
     current_image = get_object_or_404(HomePageCarouselImages, pk=current_image_pk)
     potential_images = current_image.page.carousel_images.all()
     for index, image in enumerate(potential_images):
@@ -12,6 +37,5 @@ def next_carousel_image(request, current_image_pk):
             })
         elif current_image == image:
             return render(request, 'home/carousel_image.html', {
-                'carousel_image': potential_images[index + 1],
+                'carousel_image': potential_images[index - 1],
             })
-
