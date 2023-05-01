61c063934aba05ec4ee3bb5dce1b25e7b108e868
Hans Kelson <hans@communitycomputing.net>
2023-05-01 12:30:20 -0400

    Remove unused duplicate function

--- uubloomington/home/views.py
+++ uubloomington/home/views.py
@@ -25,17 +25,3 @@ def next_carousel_image(request, current_image_pk, mode):
                                                    }[mode]
                                                    ],
             })
-
-
-def previous_carousel_image(request, current_image_pk):
-    current_image = get_object_or_404(HomePageCarouselImages, pk=current_image_pk)
-    potential_images = current_image.page.carousel_images.all()
-    for index, image in enumerate(potential_images):
-        if current_image == current_image.page.carousel_images.last():
-            return render(request, 'home/carousel_image.html', {
-                'carousel_image': potential_images[0],
-            })
-        elif current_image == image:
-            return render(request, 'home/carousel_image.html', {
-                'carousel_image': potential_images[index - 1],
-            })
