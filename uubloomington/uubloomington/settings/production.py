a7dfb1a8c31be4e3db7edd27b821173f40b23933
Hans Kelson <hans@communitycomputing.net>
2023-05-05 12:55:23 -0400

    Move to JSON for storing configuration, because Passenger's handling of env variables is horrible

--- uubloomington/uubloomington/settings/production.py
+++ uubloomington/uubloomington/settings/production.py
@@ -1,22 +1,25 @@
 from .base import *
 import os
+import json
 
 DEBUG = False
+with open(os.getenv("UUBLOOMINGTON_CONFIG_PATH")) as config_file:
+    loaded_configuration = json.load(config_file)
 
-STATIC_ROOT = os.getenv('UUBLOOMINGTON_STATIC_ROOT')
+STATIC_ROOT = loaded_configuration['static_root']
 
-MEDIA_ROOT = os.getenv('UUBLOOMINGTON_MEDIA_ROOT')
+MEDIA_ROOT = loaded_configuration['media_root']
 
-SECRET_KEY = os.getenv('UUBLOOMINGTON_SECRET_KEY')
+SECRET_KEY = loaded_configuration['secret_key']
 
-PLANNING_CENTER_APPLICATION_ID = os.getenv("UUBLOOMINGTON_PLANNING_CENTER_APPLICATION_ID")
-PLANNING_CENTER_SECRET = os.getenv("UUBLOOMINGTON_PLANNING_CENTER_SECRET")
+PLANNING_CENTER_APPLICATION_ID = loaded_configuration['planning_center_app_id']
+PLANNING_CENTER_SECRET = loaded_configuration['planning_center_secret']
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            "read_default_file": os.getenv('UUBLOOMINGTON_MYSQL_CONFIG_PATH')
+            "read_default_file": loaded_configuration['mysql_config_path']
         },
     }
 }
