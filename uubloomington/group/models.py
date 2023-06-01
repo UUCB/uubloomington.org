545d3642c5e9f6560af9c52052876447f11659b8
Hans Kelson <hans@communitycomputing.net>
2023-06-01 16:27:50 -0400

    Refactor group info fetching to avoid too many calls to Planning Center, add Church Center group URL

--- uubloomington/group/models.py
+++ uubloomington/group/models.py
@@ -9,8 +9,14 @@ from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from django.db.models import CharField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from django.utils.text import slugify
+from django.utils import timezone
+
+from site_settings.models import SiteWideSettings
 
 from core.models import Post
+
+import pickle
 
 class GroupsHomePage(Page):
     body = RichTextField()
@@ -39,6 +45,9 @@ class GroupPage(Page):
         on_delete=models.SET_NULL,
         related_name="+",
     )
+    group_info = models.BinaryField(null=True)  # Pickled Group
+    group_type = models.BinaryField(null=True)  # Pickled Group Type
+    last_fetched = models.DateTimeField(default=timezone.make_aware(datetime.datetime.min))
     content_panels = Page.content_panels + [
         FieldPanel('planning_center_group_id'),
         FieldPanel('summary'),
@@ -46,10 +55,31 @@ class GroupPage(Page):
         FieldPanel('show_posts'),
     ]
 
-    def group_info(self):
-        planningcenter = pypco.PCO(settings.PLANNING_CENTER_APPLICATION_ID, settings.PLANNING_CENTER_SECRET)
-        group = planningcenter.get(f'https://api.planningcenteronline.com/groups/v2/groups/{self.planning_center_group_id}')
-        return group
+    def get_context(self, request, *args, **kwargs):
+        site_settings = SiteWideSettings.load()
+        context = super().get_context(request)
+        if self.last_fetched < (
+            timezone.now()
+            - datetime.timedelta(minutes=site_settings.refresh_from_planningcenter_every)
+        ):
+            planningcenter = pypco.PCO(settings.PLANNING_CENTER_APPLICATION_ID, settings.PLANNING_CENTER_SECRET)
+            group_info = planningcenter.get(f'https://api.planningcenteronline.com/groups/v2/groups/{self.planning_center_group_id}')
+            group_type = planningcenter.get(f'https://api.planningcenteronline.com/groups/v2/group_types/{group_info["data"]["relationships"]["group_type"]["data"]["id"]}')
+            self.group_info = pickle.dumps(group_info)
+            self.group_type = pickle.dumps(group_type)
+            self.save()
+        else:
+            group_info = pickle.loads(self.group_info)
+            group_type = pickle.loads(self.group_type)
+        context['group'] = group_info
+        context['group_title'] = group_info['data']['attributes']['name']
+        context['group_body'] = group_info['data']['attributes']['description']
+        context['header_image'] = group_info['data']['attributes']['header_image']['original']
+        group_type_slug = slugify(group_type['data']['attributes']['name'])
+        group_slug = slugify(context['group_title'])
+        # TODO: Add a setting for churchcenter base URL and un-hard-code this
+        context['churchcenter_group_url'] = f'https://uucb.churchcenter.com/groups/{group_type_slug}/{group_slug}/'
+        return context
 
     def get_posts(self):
         return self.get_children().type(Post)
