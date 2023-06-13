ab5cb332271aecc363fefbcd5d1ac42073f137ac
Hans Kelson <hans@communitycomputing.net>
2023-06-13 12:24:42 -0400

    Add page feature template tag

---
+++ uubloomington/core/templatetags/feature_page.py
@@ -0,0 +1,14 @@
+from django import template
+from wagtail import blocks
+
+register = template.Library()
+
+
+@register.inclusion_tag('core/page_feature_block_snippet.html')
+def feature_page(page):
+    context = {
+        'value': page
+    }
+    if type(page.specific.body) == blocks.StreamValue:
+        context['body_is_streamfield'] = True
+    return context
