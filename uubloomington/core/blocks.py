3b5b4b621087587dc12bf7a14ccc99b6c16cae6d
Hans Kelson <hkelson@uubloomington.org>
2024-11-27 12:38:06 -0500

    Undo child block nonsense in core/blocks

--- uubloomington/core/blocks.py
+++ uubloomington/core/blocks.py
@@ -203,7 +203,7 @@ class BaseSectionBlock(blocks.StructBlock):
         ('auto_index', AutoIndexBlock()),
         ('selectable_index', IndexBlock()),
         ('document_list', DocumentListBlock()),
-        ('badge_area', BadgeAreaBlock(child_block=BadgeBlock())),
+        ('badge_area', BadgeAreaBlock(BadgeBlock())),
         ('anchor', AnchorBlock()),
         ('upcoming_service', UpcomingServiceBlock()),
         ('upcoming_oos', UpcomingOrderOfServiceBlock()),
