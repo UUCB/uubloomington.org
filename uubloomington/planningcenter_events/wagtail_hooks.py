d33678452838fa2b89e2c039c5497d3077df171c
Hans K <hans@communitycomputing.net>
2025-02-11 21:22:43 -0500

    Event Listings: Fix admin menu

--- uubloomington/planningcenter_events/wagtail_hooks.py
+++ uubloomington/planningcenter_events/wagtail_hooks.py
@@ -12,6 +12,8 @@ class EventListingViewSet(SnippetViewSet):
         FieldPanel('description_must_contain'),
         FieldPanel('show_next_events'),
     ]
+    add_to_admin_menu = True
+    icon = "calendar"
 
 
 register_snippet(EventListingViewSet)
