01c4265a7cf3550beba64569788b6a336f1dfb09
Hans K <hans@communitycomputing.net>
2025-08-18 13:07:38 -0400

    core.embed_finders: Remove old print debugging

--- uubloomington/core/embed_finders.py
+++ uubloomington/core/embed_finders.py
@@ -24,7 +24,6 @@ class VimeoChatEmbedFinder(EmbedFinder):
 
         This is the part that may make requests to external APIs.
         """
-        print(url)
         try:
             options = url.split('||')[1]
         except IndexError:
