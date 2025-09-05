fbea5a6e0c52e75dc0fbcd5b3974c524429922c8
Hans K <hans@communitycomputing.net>
2025-09-05 09:13:13 -0400

    WIP API Work

--- uubloomington/uubloomington/api.py
+++ uubloomington/uubloomington/api.py
@@ -6,6 +6,7 @@ from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from rest_framework.response import Response
+from rest_framework import serializers
 
 from services.models import ServicePage
 from services.blocks import OOSElementBlock
@@ -13,8 +14,32 @@ from services.blocks import OOSElementBlock
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
 
+class ServicesShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServicePage
+
 class ServicesAPIViewSet(PagesAPIViewSet):
     model = ServicePage
+
+    def listing_view(self, request):
+        available_services = ServicePage.objects.all()
+        output = {
+            'meta': {
+                "total_count": available_services.count(),
+            },
+            "items": {
+                (
+                    {
+                        "id": servicepage.pk,
+                        "meta": {
+                            "detail_url": f"{}"
+                        }
+                    }
+                    for servicepage in available_services
+                ),
+            }
+        }
+        return Response(output)
 
     def detail_view(self, request, *args, **kwargs):
         # implement some "get the OOS fields" nonsense here, pushing out some easy-to-parse JSON which will work for OpenShow
@@ -32,6 +57,7 @@ class ServicesAPIViewSet(PagesAPIViewSet):
             'name': service_page.title,
             'description': service_page.body,
             'date': service_page.order_of_service.first().date,
+            'id': service_page.pk,
             'segments': segments,
         }
         return Response(show_dict)
